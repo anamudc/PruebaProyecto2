1f23703dc88aaf778f39dbefe4f7110ac0c65cd8
anamudc <anamudc>
2012-10-07 01:51:38 -0500

    devolvi los atributos entrenador

--- TPIcardio/grails-app/domain/tpicardio/Entrenador.groovy
+++ TPIcardio/grails-app/domain/tpicardio/Entrenador.groovy
@@ -2,7 +2,9 @@ package tpicardio
 
 class Entrenador extends Persona implements Serializable{
 
-    
+	String campoLaboral
+	static hasMany = [cita:Cita,tip:Tip,ejercicio:Ejercicio,maquina:Maquina]
+	
     static constraints = {
     }
     String toString(){
